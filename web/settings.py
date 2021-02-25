f343cd75702408f6fe563da7ee246ec1faba493e
SteveSpec <wambuastephen001@gmail.com>
2021-02-25 08:58:42 +0300

    user registration form

--- web/settings.py
+++ web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'apex.apps.ApexConfig',
     'django.contrib.admin',
     'django.contrib.auth',
