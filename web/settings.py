aca7365a8e2457d426f7276563b2a46f2cd81d88
SteveSpec <wambuastephen001@gmail.com>
2021-03-03 18:01:18 +0300

    updated hm pg

--- web/settings.py
+++ web/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
-    'apex.apps.ApexConfig',
+    'apex.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
